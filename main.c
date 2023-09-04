c2745295f96276314365cc44cf08e88d6f61f967
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-04 13:44:35 +0000

    copied program

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
+// Further examples of Pointers
+
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+
+  char c = 'Q';
+  char *char_pointer = &c;
+  
+  printf ("%c %c\n", c, *char_pointer);
+
+  c = '/';
+  printf ("%c %c\n", c, *char_pointer);
+
+  *char_pointer = '{';
+  printf ("%c %c\n", c, *char_pointer);
+  
   return 0;
 }
